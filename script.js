28ea0639594d2bc673bcf0ff2fa87c1d6c4a2724
ryzecous <r3j9c89@gmail.com>
2020-12-17 19:08:17 -0500

    search button function

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ var currentCity = 'Chicago'
 var recentCities = ['Chicago']
 var apiKey = 'c9ef626d3d26fe5016c7a097d15877da'
 
+document.getElementById('searchBtn').addEventListener("click", newCitySearch)
+
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} - current weather
 var currentWeatherCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&appid=' + apiKey
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key} - 5 day forecast
 var fiveDayCall = 'https://www.api.openweathermap.org/data/2.5/forecast?q=' + currentCity + "&appid=" + apiKey
 
+// API call on page load
+
+// On click search button function
+function newCitySearch() {
+    var searchText = document.getElementById ('citySearchForm').innerText
+    currentCity = searchText
+    e.preventDefault()
+    console.log(currentCity)
+}
